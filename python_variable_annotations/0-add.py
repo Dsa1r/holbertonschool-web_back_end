72575c928ac61e0dc753818b2d7b3a452c52409e
Dsa1r <danamusaad22@gmail.com>
2026-08-26 19:53:16 +0300

    Update docstring in 0-add.py
    Shortened the module docstring for clarity.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""This module provides a function `add` that adds two floats and returns the sum."""
+"""This module provides a function `add`"""
+
+
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
