06fab81e1034c217109d34c948ae7e9fdb595c87
Dsa1r <danamusaad22@gmail.com>
2026-08-26 19:52:14 +0300

    Add type annotations to add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""This module provides a function `add` that adds two floats and returns the sum."""
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
