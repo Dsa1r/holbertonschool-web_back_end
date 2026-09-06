7fa597c106f03137ae8e3e6d9f6a12ad5b0d99b4
dsa1r <Danamusaad22@gmail.com>
2026-09-06 16:55:55 +0300

    create index range method

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Build a helper method to show the first and end index of the page
+"""
+def index_range(page, page_size):
+    """
+    Calculate the start and end index for a given page and page size.
+
+    Args:
+        page (int): The current page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
