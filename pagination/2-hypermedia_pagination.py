3f69f3fdbf577123515a637c3ca488a295e444a0
dsa1r <Danamusaad22@gmail.com>
2026-09-10 17:38:03 +0300

    task3

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -43,3 +43,25 @@ class Server:
 
         start_index, end_index = index_range(page, page_size)
         return self.dataset()[start_index:end_index]
+
+    def get_huper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get a hypermedia page of the dataset.
+
+        Args:
+            page (int): The current page number.
+            page_size (int): The number of items per page.
+
+        Returns:
+            dict: A dictionary containing the requested page of data and pagination information.
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages  
+        }
+    
