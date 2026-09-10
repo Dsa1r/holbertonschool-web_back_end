ea201a8640584c9d8dd68d2d42d75dc322803480
dsa1r <Danamusaad22@gmail.com>
2026-09-10 16:59:21 +0300

    a

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,23 +2,43 @@
 """
 Build a helper method to get the page
 """
+import csv
+import math
+from typing import List
 
-def get_page (page):
-    assert isinstance(page, int) and page > 0, "page must be a positive integer"
-    
+index_range = __import__('0-simple_helper_function').index_range
 
-
-def index_range(page, page_size):
+class Server:
+    """Server class to paginate a database of popular baby names.
     """
-    Calculate the start and end index for a given page and page size.
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-    Args:
-        page (int): The current page number.
-        page_size (int): The number of items per page.
+    def __init__(self):
+        self.__dataset = None
 
-    Returns:
-        tuple: A tuple containing the start index and end index.
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of the dataset.
+
+        Args:
+            page (int): The current page number.
+            page_size (int): The number of items per page.
+
+        Returns:
+            List[List]: A list of lists representing the requested page of data.
+        """
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
